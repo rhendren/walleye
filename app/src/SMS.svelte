eadcddc6b839cefe1a71ccaf89773ae936ac579f
rhendren <rhendren@better.com>
2020-09-28 14:58:16 -0400

    Create public build

--- app/src/SMS.svelte
+++ app/src/SMS.svelte
@@ -46,56 +46,6 @@
     }
 </script>
 
-<!-- <script>
-    import {encode} from 'base-64';
-    const fetch = require('node-fetch');
-
-    export let txtMessage;
-    export let filterSettings;
-    export let customerData;
-
-    function requestTexts(e) {
-        // Need to filter using filterSettings
-        console.log(filterSettings);
-        customerData.forEach(async (person) => {
-            await textRequest(person.phone, txtMessage);
-        });
-
-        async function textRequest(number, txtMessage) {
-            console.log("trying");
-            txtMessage = "Something";
-            const username = 'c1d2c6ea2510';
-            const password = '8a67b000295d56f8';
-            const url = "https://api.callfire.com/v2/texts";
-            console.log("Making request");
-            console.log(number);
-            console.log(txtMessage);
-
-            await fetch(url, {
-                method: 'POST',
-                "headers": {
-                    'Content-Ty    34wpe': 'application/json',
-                    'Authorization': 'Basic ' + encode(username + ":" + password)
-                },
-                "body": JSON.stringify([{
-                    phoneNumber: number,
-                    message: txtMessage
-                }])
-            })
-            .then((response) => {
-                console.log(response.status);
-                console.log(response.data);
-                return;
-            })
-            .catch((error) => {
-                console.log(error);
-                return;
-            })
-        }
-    }
-
-</script> -->
-
 <button on:click|once={sendText}>Submit</button>
 
 {#each customerNumbers as num}
